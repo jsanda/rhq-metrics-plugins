3923d4e5d22a4e97f778bc858ff5c31e295a6281
John Sanda <jsanda@redhat.com>
2012-08-07 15:49:14 -0400

    adding logging

--- perf-tests/src/test/java/org/rhq/server/plugins/metrics/perf/MetricsPluginTest.java
+++ perf-tests/src/test/java/org/rhq/server/plugins/metrics/perf/MetricsPluginTest.java
@@ -5,6 +5,7 @@ import static org.joda.time.DateTime.now;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,6 +18,8 @@ import org.rhq.enterprise.server.plugin.pc.metrics.MetricsServerPluginTestDelega
  * @author John Sanda
  */
 public class MetricsPluginTest {
+
+    private Logger log = Logger.getLogger(MetricsPluginTest.class);
 
     private MetricsServerPluginFacet  metricsServer;
 
@@ -31,15 +34,21 @@ public class MetricsPluginTest {
         testDelegate = (MetricsServerPluginTestDelegate) testDelegateClass.newInstance();
 
         metricsServer.initialize(testDelegate.createTestContext());
+
+        log.debug("Initialized " + metricsServer.getClass().getName());
     }
 
     @Test
     public void insertRawMetrics() {
+        log.debug("starting insertRawMetrics()");
+
         int numReports = 10;
         int numSchedulesPerReport = 10;
 
         DateTime startTime = now().minusDays(1);
         DateTime collectionTime = startTime;
+
+        long start = System.currentTimeMillis();
 
         for (int i = 0; i < numReports; ++i) {
             Set<MeasurementDataNumeric> data = new HashSet<MeasurementDataNumeric>();
@@ -50,6 +59,9 @@ public class MetricsPluginTest {
             collectionTime = collectionTime.plusSeconds(10);
             metricsServer.addNumericData(data);
         }
+
+        long end = System.currentTimeMillis();
+        log.debug("Finished inserting raw metrics in " + (end - start) + " ms");
     }
 
 }
